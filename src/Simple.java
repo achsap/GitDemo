0a21453f752b9e904e6523a89c3f9d2ecf6fc8d7
achsap <ACHSAH-PREETHI.P@CAPGEMINI.COM>
2024-12-18 09:58:21 +0530

    Update Simple.java

--- src/Simple.java
+++ src/Simple.java
@@ -3,5 +3,6 @@ public class Simple {
 		System.out.println("message 1 from sts");
 		System.out.println("message 2 from git");
 		System.out.println("message 3 from sts from first branch");
+		System.out.println("message 4 from github from first branch");
 	}
 }
