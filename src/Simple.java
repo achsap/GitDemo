af2daa457cb324812d5be7c4c17814b9e2cc3f46
P <achsah-preethi.p@capgemini.com>
2024-12-18 09:55:02 +0530

    fourth commit

--- src/Simple.java
+++ src/Simple.java
@@ -2,5 +2,6 @@ public class Simple {
 	public static void main(String[] args) {
 		System.out.println("message 1 from sts");
 		System.out.println("message 2 from git");
+		System.out.println("message 3 from sts from first branch");
 	}
 }
